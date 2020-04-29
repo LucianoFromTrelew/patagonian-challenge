0e7cac43a86149f79203e84dca06693bd59ad09d
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2020-04-29 19:19:21 -0300

    Display error message at entry point

--- src/load_data/index.ts
+++ src/load_data/index.ts
@@ -1,5 +1,6 @@
 import { main } from "./main";
 
-main(process.argv.slice(2)).catch(() => {
+main(process.argv.slice(2)).catch((err: Error) => {
+  console.log(err.message);
   process.exitCode = 1;
 });
