e665bd5bdb170cfa3cd0df8c88cf85c4cf594d41
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2020-04-30 21:45:02 -0300

    Move controller functions to separate file

--- src/app.ts
+++ src/app.ts
@@ -1,54 +1,12 @@
-import express, { Request, Response, NextFunction } from "express";
-import cors from "cors";
+import express from "express";
 import { Connection } from "typeorm";
-import { Artist } from "./models/artist.model";
-import { Song } from "./models/song.model";
+import { getSongs, getSongById } from "./controllers/songs.controller";
 
 export async function getApp(conn: Connection) {
   const app = express();
 
-  // CORS
-  app.use(cors());
-
-  app.get("/songs", async (req, res) => {
-    const { artistName } = req.query;
-    if (!artistName)
-      return res.status(400).send({
-        message: "Bad request - `artistName` query parameter must be present"
-      });
-    if (artistName.length < 3)
-      return res.status(400).send({
-        message:
-          "Bad request - `artistName` query parameter should be at least 3 characters long"
-      });
-    const name = (artistName as string).toLowerCase();
-    const artistWithSongs = await conn
-      .getRepository(Artist)
-      .createQueryBuilder("artist")
-      .select()
-      .where("artist.name ILIKE :name", { name: `%${name}%` })
-      .leftJoinAndSelect("artist.songs", "song")
-      .getOne();
-    return res.send({
-      songs: artistWithSongs?.songs.map(song => ({
-        songId: song.id,
-        songTitle: song.name
-      }))
-    });
-  });
-  app.get(
-    "/songs/:songId",
-    async (req: Request, res: Response, next: NextFunction) => {
-      const { songId } = req.params;
-      try {
-        const song = await conn.getRepository(Song).findOneOrFail(songId);
-        const { available_markets, ...data } = JSON.parse(song.fullResponse);
-        return res.send(data);
-      } catch {
-        return res.status(404).send({ message: "Song not found" });
-      }
-    }
-  );
+  app.get("/songs", getSongs(conn));
+  app.get("/songs/:songId", getSongById(conn));
 
   return app;
 }
---
+++ src/controllers/songs.controller.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from "express";
+import { Connection } from "typeorm";
+import { Artist } from "../models/artist.model";
+import { Song } from "../models/song.model";
+
+export function getSongs(conn: Connection) {
+  return async (req: Request, res: Response) => {
+    const { artistName, limit = "50", page = "1" } = req.query;
+    if (!artistName)
+      return res.status(400).send({
+        message: "Bad request - `artistName` query parameter must be present"
+      });
+    if (artistName.length < 3)
+      return res.status(400).send({
+        message:
+          "Bad request - `artistName` query parameter should be at least 3 characters long"
+      });
+    const name = (artistName as string).toLowerCase();
+    const artist = await Artist.searchByName(conn, name);
+    if (!artist)
+      return res
+        .status(404)
+        .send({ message: `Artist [${artistName}] not found` });
+    const limitPage = parseInt(limit as string);
+    const pageNumber = parseInt(page as string) * limitPage;
+    try {
+      const songs = await conn
+        .getRepository(Song)
+        .createQueryBuilder("song")
+        .leftJoinAndSelect("song.artist", "artist")
+        .where({ artist })
+        .orderBy("song.id")
+        .take(limitPage)
+        .skip(pageNumber)
+        .getMany();
+      return res.send({
+        songs: songs.map(song => ({
+          songId: song.id,
+          songTitle: song.name
+        }))
+      });
+    } catch {
+      return res.status(400).send({
+        message:
+          "Bad request - `page` or `limit` query parameter should be numbers"
+      });
+    }
+  };
+}
+
+export function getSongById(conn: Connection) {
+  return async (req: Request, res: Response) => {
+    const { songId } = req.params;
+    try {
+      const song = await conn.getRepository(Song).findOneOrFail(songId);
+      const { available_markets, ...data } = JSON.parse(song.fullResponse);
+      return res.send(data);
+    } catch {
+      return res.status(404).send({ message: "Song not found" });
+    }
+  };
+}
