5db6c1c9f43c400e30a3d6f3349f461294a5b110
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2020-04-30 21:45:24 -0300

    Update tests

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -12,6 +12,7 @@ import { getApp } from "../app";
 describe("App", () => {
   let conn: Connection;
   let app: Application;
+
   beforeAll(async () => {
     const envConfig = parse(readFileSync(".env.test"));
     for (const key in envConfig) {
@@ -20,7 +21,7 @@ describe("App", () => {
     conn = await getConnection("test");
     const duaLipa = Artist.create("dua_lipa", "Dua Lipa", "", "");
     const jBalvin = Artist.create("j_balvin", "J Balvin", "", "");
-    const futureNostaliga = Album.create(
+    const futureNostalgia = Album.create(
       "future_nostalgia",
       "Future Nostalgia",
       "",
@@ -28,22 +29,31 @@ describe("App", () => {
       duaLipa
     );
     const colores = Album.create("colores", "Colores", "", "", jBalvin);
-    const physical = Song.create(
-      "physical",
-      "Physical",
-      '{"id": "physical"}',
-      futureNostaliga
-    );
-    const rojo = Song.create("rojo", "Rojo", '{"id": "rojo"}', colores);
+    const duaLipasSongs = Array(100)
+      .fill(0)
+      .map((_: any, i: number) => {
+        const id = `${String(i).padStart(3, "0")}_dua_lipa`;
+        return Song.create(id, `Song ${i}`, `{"id": "${id}"}`, futureNostalgia);
+      });
+    const jBalvinsSongs = Array(100)
+      .fill(0)
+      .map((_: any, i: number) => {
+        const id = `${String(i).padStart(3, "0")}_j_balvin`;
+        return Song.create(id, `Song ${i}`, `{"id": "${id}"}`, colores);
+      });
     await Promise.all([duaLipa, jBalvin].map(v => conn.manager.save(v)));
     await Promise.all(
-      [futureNostaliga, colores].map(v => conn.manager.save(v))
+      [futureNostalgia, colores].map(v => conn.manager.save(v))
     );
-    await Promise.all([physical, rojo].map(v => conn.manager.save(v)));
+    await Promise.all(
+      [...duaLipasSongs, ...jBalvinsSongs].map(v => conn.manager.save(v))
+    );
   });
+
   beforeEach(async () => {
     app = await getApp(conn);
   });
+
   afterAll(async () => {
     await conn.dropDatabase();
     await conn.close();
@@ -55,29 +65,107 @@ describe("App", () => {
       expect(res.status).toBe(400);
       expect(res.body.message).toMatch(/must be present/);
     });
+
     test("returns status code 400 if artist name is shorter than 3 characters long", async () => {
       const res = await request(app).get("/songs").query({ artistName: "du" });
       expect(res.status).toBe(400);
       expect(res.body.message).toMatch(/at least 3 characters long/);
     });
+
     test("returns artist's songs", async () => {
       const duaLipa = await conn
         .getRepository(Artist)
         .findOne({ relations: ["songs"], where: { name: "Dua Lipa" } });
       const res = await request(app).get("/songs").query({ artistName: "dua" });
       expect(res.status).toBe(200);
-      expect(res.body.songs).toHaveLength(duaLipa?.songs.length as number);
-      expect(res.body.songs).toMatchObject([
-        { songId: duaLipa?.songs[0].id, songTitle: duaLipa?.songs[0].name }
-      ]);
+      expect(res.body.songs).toHaveLength(50);
+    });
+
+    test("return 404 if artist not found", async () => {
+      const res = await request(app)
+        .get("/songs")
+        .query({ artistName: "red hot chili peppers" });
+      expect(res.status).toBe(404);
+      expect(res.body.message).toMatch(/not found/);
+    });
+
+    test("returns only 10 songs if limit = 10", async () => {
+      const limit = 10;
+      const duaLipa = await conn
+        .getRepository(Artist)
+        .findOne({ where: { name: "Dua Lipa" } });
+      const res = await request(app)
+        .get("/songs")
+        .query({ artistName: "dua", limit });
+      expect(res.status).toBe(200);
+      expect(res.body.songs).toHaveLength(limit);
+    });
+
+    test("returns paginated data", async () => {
+      const page = 1;
+      const duaLipa = await conn
+        .getRepository(Artist)
+        .findOne({ name: "Dua Lipa" });
+      const songs = ((await conn
+        .getRepository(Song)
+        .createQueryBuilder("song")
+        .leftJoinAndSelect("song.artist", "artist")
+        .where({ artist: duaLipa })
+        .orderBy("song.id")
+        .getMany()) as Song[]).slice(50);
+      const res = await request(app)
+        .get("/songs")
+        .query({ artistName: "dua", page });
+      expect(res.status).toBe(200);
+      expect(res.body.songs[0].songId).toBe(songs[0].id);
+    });
+
+    test("returns paginated data with limit", async () => {
+      const page = 2;
+      const limit = 10;
+      const duaLipa = await conn
+        .getRepository(Artist)
+        .findOne({ name: "Dua Lipa" });
+      const songs = (await conn
+        .getRepository(Song)
+        .createQueryBuilder("song")
+        .leftJoinAndSelect("song.artist", "artist")
+        .where({ artist: duaLipa })
+        .orderBy("song.id")
+        .getMany()) as Song[];
+      const res = await request(app)
+        .get("/songs")
+        .query({ artistName: "dua", page, limit });
+      expect(res.status).toBe(200);
+      expect(res.body.songs[0].songId).toBe(songs[page * limit].id);
+    });
+
+    test("returns 400 if page query param is not a number", async () => {
+      const page = "somePage";
+      const res = await request(app)
+        .get("/songs")
+        .query({ artistName: "dua", page });
+      expect(res.status).toBe(400);
+      expect(res.body.message).toMatch(/Bad request/);
+    });
+
+    test("returns 400 if page query param is not a number", async () => {
+      const limit = "limit";
+      const res = await request(app)
+        .get("/songs")
+        .query({ artistName: "dua", limit });
+      expect(res.status).toBe(400);
+      expect(res.body.message).toMatch(/Bad request/);
     });
   });
+
   describe("/songs/:songId endpoint", () => {
     test("returns 404 if song not found", async () => {
       const res = await request(app).get("/songs/someNonexistingSong");
       expect(res.status).toBe(404);
       expect(res.body.message).toMatch(/Song not found/);
     });
+
     test("returns song", async () => {
       const song = (await conn.getRepository(Song).findOne()) as Song;
       const { available_markets, ...data } = JSON.parse(song.fullResponse);
